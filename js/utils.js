8630e78c42d4f38238cd17f89dc1e1193cc3ed4a
andriiprotsko <andrii.protsko@icloud.com>
2022-09-09 15:00:55 +0300

    Add files via upload

--- js/utils.js
+++ js/utils.js
@@ -12,6 +12,10 @@ function checkPageMode() {
     if (pageMode == 'dark') body.classList.add("dark-mode")
     else body.classList.remove("dark-mode");
 
+    const ppageMode = window.localStorage.getItem("hardcoreMode");
+    if (ppageMode == 'true') body.classList.add("hardcore-mode")
+    else body.classList.remove("hardcore-mode");
+
     const autoLevelStart = window.sessionStorage.getItem("reloading");
     if (autoLevelStart == "true")
     {
@@ -22,16 +26,30 @@ function checkPageMode() {
 
 function resetProgress() {
     window.localStorage.setItem("currentLevel", "01");
+    window.sessionStorage.setItem("reloading", "false");
     window.location.reload(false);
 }
 
-function runLevel() {
+function startLevel() {
+    window.sessionStorage.setItem("reloading", "true");
     const body = document.body;
     body.classList.add('show-level');
- }
-
+}
 
  function homeLink() {
-    window.sessionStorage.setItem("reloading", "");
+    window.sessionStorage.setItem("reloading", "false");
     window.location.reload(false);
+}
+
+function restartLevel() {
+    if (window.sessionStorage.getItem("reloading") == 'true') window.location.reload(false);
+}
+
+function changeHardcoreMode() {
+    const body = document.body;
+    var checkHardcoreMode = window.localStorage.getItem("hardcoreMode");
+    if (checkHardcoreMode == 'true') window.localStorage.setItem("hardcoreMode", "false");
+    else window.localStorage.setItem("hardcoreMode", "true");
+    body.classList.toggle("hardcore-mode");
+    window.location.reload(false)
 }
